07cc5f4dc2d922252c8289c3491effc7c418f5cb
Kevin Cook <140981683+Kevin-T-Cook@users.noreply.github.com>
2024-05-28 22:37:49 -0700

    Initial Setup

---
+++ Server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 8080;
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(
+	"/",
+	express.static(path.join(__dirname, "public"))
+);
+
+app.use(express.json());
+app.use(
+	express.urlencoded({ limit: "50mb", extended: true })
+);
+
+// app.use("/auth", require("./auth"));
+
+// app.use("/api", require("./api"));
+
+app.listen(PORT, () => {
+	console.log("\n LISTENING ON PORT " + PORT);
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res
+		.status(err.status || 500)
+		.send(err.message || "Internal server error.");
+});
